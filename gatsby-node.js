9d306ab8fa6643e3f66aa0d833bf3e67fe4959c1
Ryan Harris <harrisryan1@gmail.com>
2019-11-08 19:13:44 -0500

    Add filter to allMdx query

--- gatsby-node.js
+++ gatsby-node.js
@@ -8,7 +8,7 @@ exports.createPages = async ({ graphql, actions }) => {
   const result = await graphql(
     `
       {
-        allMdx {
+        allMdx(filter: { fileAbsolutePath: { regex: "/blog/" } }) {
           edges {
             node {
               frontmatter {
@@ -34,26 +34,20 @@ exports.createPages = async ({ graphql, actions }) => {
   // Create blog posts pages.
   const posts = result.data.allMdx.edges
 
-  posts
-    .filter(post => {
-      // We want to exclude our MDX pages when generating our blog posts
-      const regex = new RegExp("/pages/")
-      return !regex.test(post.node.fileAbsolutePath)
-    })
-    .forEach(post => {
-      // const previous = index === posts.length - 1 ? null : posts[index + 1].node
-      // const next = index === 0 ? null : posts[index - 1].node
+  posts.forEach(post => {
+    // const previous = index === posts.length - 1 ? null : posts[index + 1].node
+    // const next = index === 0 ? null : posts[index - 1].node
 
-      createPage({
-        path: post.node.fields.slug,
-        component: blogPost,
-        context: {
-          slug: post.node.fields.slug,
-          // previous,
-          // next,
-        },
-      })
+    createPage({
+      path: post.node.fields.slug,
+      component: blogPost,
+      context: {
+        slug: post.node.fields.slug,
+        // previous,
+        // next,
+      },
     })
+  })
 }
 
 exports.onCreateNode = ({ node, actions, getNode }) => {
