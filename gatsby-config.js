2283213afc8ec4cda667f734dfd4df750bb77ca8
Ryan Harris <harrisryan1@gmail.com>
2019-12-01 12:56:54 -0500

    Add GA id to gatsby-config

--- gatsby-config.js
+++ gatsby-config.js
@@ -70,7 +70,7 @@ module.exports = {
     {
       resolve: `gatsby-plugin-google-analytics`,
       options: {
-        //trackingId: `ADD YOUR TRACKING ID HERE`,
+        trackingId: `UA-135453133-1`,
       },
     },
     // `gatsby-plugin-feed`,
