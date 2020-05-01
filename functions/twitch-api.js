0fb79b75c022a4d356282509a48cb3dd1cda8ee5
Ryan Harris <harrisryan1@gmail.com>
2020-05-01 18:30:45 -0400

    fix twitch-api function

--- functions/twitch-api.js
+++ functions/twitch-api.js
@@ -5,53 +5,49 @@ const TWITCH_API = "https://id.twitch.tv/oauth2/token"
 
 function getTwitchData(token) {
   console.log("Fetching broadcast info...")
-  console.log(`---> Using token: ${token}`)
 
-  return fetch("https://api.twitch.tv/helix/streams?user_login=ryan_c_harris", {
+  const apiUrl = "https://api.twitch.tv/helix/streams?user_login=ryan_c_harris"
+
+  return fetch(apiUrl, {
     method: "GET",
-    header: {
+    headers: {
       Authorization: `Bearer ${token}`,
-      "Client-ID": process.env.GATSBY_TWITCH_CLIENT_ID,
     },
   })
-    .then(res => res.json())
-    .then(json => json)
+    .then(res => {
+      if (res.ok) {
+        return res.json()
+      } else {
+        throw new Error("Unable to authenticate with Twitch API")
+      }
+    })
     .catch(err => err)
 }
 
 exports.handler = async function(event, context) {
   console.log("Requesting token from Twitch API...")
-  const data = fetch(
-    `${TWITCH_API}?client_id=${process.env.GATSBY_TWITCH_CLIENT_ID}&client_secret=${process.env.GATSBY_TWITCH_CLIENT_SECRET}&grant_type=client_credentials`,
-    {
-      method: "POST",
-      headers: {
-        Accept: "application/json",
-      },
-    }
-  )
-    .then(res => res.json())
-    .then(json => {
-      const token = json.access_token
-      console.log("---> Token response:")
-      console.log(json)
-      return getTwitchData(token)
-    })
-    .then(twitch => {
-      const { status } = twitch
 
-      if (status === 200) {
-        console.log("Success!")
-        return {
-          statusCode: 200,
-          body: JSON.stringify(twitch),
-        }
+  const tokenUrl = `${TWITCH_API}?client_id=${process.env.GATSBY_TWITCH_CLIENT_ID}&client_secret=${process.env.GATSBY_TWITCH_CLIENT_SECRET}&grant_type=client_credentials`
+
+  const data = fetch(tokenUrl, {
+    method: "POST",
+    headers: {
+      Accept: "application/json",
+      "Client-ID": process.env.GATSBY_TWITCH_CLIENT_ID,
+    },
+  })
+    .then(res => {
+      if (res.ok) {
+        return res.json()
       } else {
-        console.log("Request failed...")
-        return {
-          statusCode: status,
-          body: `${twitch.error} [${status}]: ${twitch.message}`,
-        }
+        throw new Error("Cannot retrieve access_token from Twitch API")
+      }
+    })
+    .then(async json => {
+      const token = json.access_token
+      return {
+        statusCode: 200,
+        body: JSON.stringify(await getTwitchData(token)),
       }
     })
     .catch(err => {
